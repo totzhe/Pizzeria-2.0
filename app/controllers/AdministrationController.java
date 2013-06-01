cea4eda538c2331244f940ec6bd04e97605655e8
altaos <alishka.man@gmail.com>
2013-06-02 00:43:07 +0400

    1

--- app/controllers/AdministrationController.java
+++ app/controllers/AdministrationController.java
@@ -3,9 +3,14 @@ package controllers;
 import daos.AdminDao;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
+import services.ShowMenuService;
 import views.html.indexadmin;
+import views.html.menu;
 import views.html.orders;
+
+import java.io.File;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,8 +21,10 @@ import views.html.orders;
  */
 public class AdministrationController extends Controller {
 
-    public static Result indexadmin() {
-        return ok(indexadmin.render());
+    @Transactional
+    public static Result indexadmin(int id) {
+        return ok(indexadmin.render(id, new ShowMenuService().getDishSorts(),
+                new ShowMenuService().getDishSortById(id).getDishes()));
     }
 
     @Transactional
@@ -29,7 +36,14 @@ public class AdministrationController extends Controller {
             return badRequest("Login or password is wrong");
     }
 
+    @Transactional
+    public static Result addDish(int id) {
+        return ok(indexadmin.render(id, new ShowMenuService().getDishSorts(),
+                new ShowMenuService().getDishSortById(id).getDishes()));
+    }
+
     public static Result orders() {
         return ok(orders.render());
     }
+
 }
--- app/forms/User.java
+++ app/forms/User.java
@@ -1,4 +1,4 @@
-package models;
+package forms;
 
 import play.data.validation.Constraints;
 
